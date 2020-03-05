8aab2eb09857daa8e94bf5e4ad5ba37c7a2d1b0d
z888zNokbindai <61818725+z888zNokbindai@users.noreply.github.com>
2020-03-05 21:30:53 +0700

    Create Exercise4_Pasuwut_P.py

---
+++ Exercise4_Pasuwut_P.py
@@ -0,0 +1,9 @@
+FE = 50.0
+GB = 80.5
+IC = 56.2
+CP = 79.8
+print("--- Your Score ---")
+print("Foundation English : ",FE)
+print("General Business : ",GB)
+print("Introduction to Conputer System : ",IC)
+print("Computer Programming : ",CP)
